945735b101ee5661db18986e335d0dd454537727
daysandmoons1 <70629221+daysandmoons1@users.noreply.github.com>
2021-03-11 18:59:13 +0900

    player movement

---
+++ Hunter_4Masters/Assets/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    CapsuleCollider2D capsuleCollider;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+    }
+}
